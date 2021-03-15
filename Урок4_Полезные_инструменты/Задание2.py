1409f53a19c9e62ae36235d9d832114141b686f8
oganesyankarina <oganesyankarina@mail.ru>
2021-03-15 19:41:55 +0300

    Практическое задание к уроку 4

--- Урок4_Полезные_инструменты/Задание2.py
+++ Урок4_Полезные_инструменты/Задание2.py
@@ -8,4 +8,3 @@
 start_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 
 print([item for index, item in enumerate(start_list[1:]) if item > start_list[index]])
-
--- Урок4_Полезные_инструменты/Задание6.py
+++ Урок4_Полезные_инструменты/Задание6.py
@@ -6,3 +6,29 @@
 Например, в первом задании выводим целые числа, начиная с 3, а при достижении числа 10 завершаем цикл.
 Во втором также необходимо предусмотреть условие, при котором повторение элементов списка будет прекращено.
 """
+from itertools import count, cycle
+
+
+def my_iterator_1(num1, num2):
+    result = []
+    for el in count(num1):
+        if el > num2:
+            break
+        else:
+            result.append(el)
+    return result
+
+
+def my_iterator_2(list_, num):
+    result = []
+    i = 0
+    for item in cycle(list_):
+        if i > num:
+            break
+        result.append(item)
+        i += 1
+    return result
+
+
+print(my_iterator_1(3, 10))
+print(my_iterator_2([1, 2, 3], 10))
